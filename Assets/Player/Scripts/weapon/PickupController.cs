13beb83261cf3153f271b66d2f89f18394cf0dd0
Royd54 <duponroy@gmail.com>
2020-11-04 19:49:31 +0100

    Added item pickups and made prefabs
    Added a weapon pickup script. This script checks if there is a weapon in range, based on a raycast. This raycast checks for weapons, when it triggers one the player can choose to pick it up. The player can also drop the weapon whenever they want to.
    The weapon also gets moved to a specific weapon layer, so that the weapons can be renderer above the map. This prevents the weapon models from going through walls! Lastly I made some prefabs for the weapon system, and added some materials.

---
+++ Assets/Player/Scripts/weapon/PickupController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    [SerializeField] private Transform _equipPos;
+    [SerializeField] private float _distance = 10f;
+
+    [SerializeField] private bool _canGrab;
+
+    private GameObject _currentWeapon;
+    private GameObject _wp;
+
+
+
+    private void Update()
+    {
+        CheckWeapons();
+
+        if (_canGrab)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (_currentWeapon != null)
+                    Drop();
+
+                PickUp();
+            }
+        }
+        if (_currentWeapon != null)
+        {
+            if (Input.GetKey(KeyCode.Q))
+                Drop();
+        }
+
+    }
+
+    private void CheckWeapons()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, _distance))
+        {
+            if (hit.transform.tag == "CanGrab")
+            {
+                _canGrab = true;
+                _wp = hit.transform.gameObject;
+            }
+        }
+        else _canGrab = false;
+    }
+
+    private void PickUp()
+    {
+        _currentWeapon = _wp;
+        foreach (Transform trans in _currentWeapon.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = 12;
+        }
+        _currentWeapon.transform.position = _equipPos.position;
+        _currentWeapon.transform.parent = _equipPos;
+        _currentWeapon.transform.eulerAngles = _equipPos.transform.eulerAngles;
+        _currentWeapon.GetComponent<Rigidbody>().isKinematic = true;
+        _currentWeapon.GetComponent<ProjectileGun>().enabled = true;
+    }
+
+    private void Drop()
+    {
+        foreach (Transform trans in _currentWeapon.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = 0;
+        }
+        _currentWeapon.transform.parent = null;
+        _currentWeapon.GetComponent<Rigidbody>().isKinematic = false;
+        _currentWeapon.GetComponent<ProjectileGun>().enabled = false;
+        _currentWeapon = null;
+    }
+}
