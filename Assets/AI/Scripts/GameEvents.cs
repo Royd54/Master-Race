1e686953e5040a506ebc161548ffc023bb4a4467
Royd54 <duponroy@gmail.com>
2020-11-05 01:29:17 +0100

    Added EnemySpawner
    This script is a first test for spawning and making a wavesystem. It spawns an enemy when the values allow it to. Also added a GameEvents script. This script uses delegates/actions to trigger functions. This way everything can stay loose from eachother.

---
+++ Assets/AI/Scripts/GameEvents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public event Action onEnemyDeath;
+    public void EnemyDeath()
+    {
+        if (onEnemyDeath != null)
+        {
+            onEnemyDeath();
+        }
+    }
+
+    public event Action onSlomo;
+    public void Slomo()
+    {
+        if (onSlomo != null)
+        {
+            onSlomo();
+        }
+    }
+
+    public event Action onLightstrigger;
+    public void LightsTrigger()
+    {
+        if (onLightstrigger != null)
+        {
+            onLightstrigger();
+        }
+    }
+}
